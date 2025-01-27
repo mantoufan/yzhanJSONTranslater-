d213f57c0d5c7adfbc49534622e5ddc68d9fa1b8
mantoufan <mhjlw@126.com>
2025-01-27 09:09:42 +0800

    :hammer: update structure of yzhantranslator

--- src/YZhanTranslator.php
+++ src/YZhanTranslator.php
@@ -8,32 +8,32 @@ class YZhanTranslator {
   private $client;
 
   public function __construct(?array $params = null) {
-    $this->client = $params['client'] ?? 'OpenAI';
-    $this->yzhanGateway = new YZhanGateway($this->client, array(
-      'apiKey' => $params['apiKey'],
+    $this->client       = $params['client'] ?? 'OpenAI';
+    $this->yzhanGateway = new YZhanGateway($this->client, [
+      'apiKey'       => $params['apiKey'],
       'organization' => $params['organization'],
-    ));
+    ]);
     $this->apiUrl = $params['apiUrl'];
   }
 
-  public function run(array $content, string $input, ?array $params = array()) {
+  public function run(array $content, string $input, ?array $params = []) {
     if (empty($params['prompt']) === false) {
       $content[0]['text'] .= '\n' . $params['prompt'];
     }
 
     $content[0]['text'] .= '\n' . $input;
 
-    $params = array_merge(array(
-      'method' => 'POST',
-      'url' => $this->apiUrl . '/v1/chat/completions',
-      'postFields' => array(
-        'model' => 'gpt-4o-mini',
-        'messages' => array(array('role' => 'user', "content" => $content)),
-      ),
-    ), $params);
+    $params = array_merge([
+      'method'     => 'POST',
+      'url'        => $this->apiUrl . '/v1/chat/completions',
+      'postFields' => [
+        'model'    => 'gpt-4o-mini',
+        'messages' => [['role' => 'user', "content" => $content]],
+      ],
+    ], $params);
 
-    $cacheParams = $params['cache'] ?? array('type' => 'File', 'params' => array());
-    $res = $this->yzhanGateway->cache($cacheParams['cache']['type'] ?? 'File', $cacheParams['params'] ?? array())->request($params);
+    $cacheParams = $params['cache'] ?? ['type' => 'File', 'params' => []];
+    $res         = $this->yzhanGateway->cache($cacheParams['cache']['type'] ?? 'File', $cacheParams['params'] ?? [])->request($params);
 
     $content = null;
 
@@ -45,21 +45,21 @@ class YZhanTranslator {
       }
     }
 
-    return array($content, $params);
+    return [$content, $params];
   }
 
-  public function translate(string $input, string $language, ?array $params = array()) {
+  public function translate(string $input, string $language, ?array $params = []) {
     if (isset($params['type'])) {
       if ($params['type'] === 'json') {
-        list($content, $params) = $this->run(array(array("type" => "text", "text" => 'Translate the values of the ' . $params['type'] . ' below into [' . $language . ']. Keep the keys unchanged, and output only the JSON string, without any markdown formatting.')), $input, $params);
+        list($content, $params) = $this->run([["type" => "text", "text" => 'Translate the values of the ' . $params['type'] . ' below into [' . $language . ']. Keep the keys unchanged, and output only the JSON string, without any markdown formatting.']], $input, $params);
       } elseif ($params['type'] === 'images') {
-        $images = json_decode($input, true);
-        list($content, $params) = $this->run(array_merge(array(
-          array("type" => "text", "text" => 'Please describe the image using [' . $language . ']. Return in the format { "' . $images[0] . '": {"description": ""}, ...}. Only include descriptions, without markdown formatting.'),
-        ), array_map(fn($image): array=> array("type" => 'image_url', "image_url" => array('url' => $image, "detail" => "high")), $images)), '', $params);
+        $images                 = json_decode($input, true);
+        list($content, $params) = $this->run(array_merge([
+          ["type" => "text", "text" => 'Please describe the image using [' . $language . ']. Return in the format { "' . $images[0] . '": {"description": ""}, ...}. Only include descriptions, without markdown formatting.'],
+        ], array_map(fn($image): array=> ["type" => 'image_url', "image_url" => ['url' => $image, "detail" => "high"]], $images)), '', $params);
       }
     } else {
-      list($content, $params) = $this->run(array(array("type" => "text", "text" => 'Translate the content below into [' . $language . '].')), $input, $params);
+      list($content, $params) = $this->run([["type" => "text", "text" => 'Translate the content below into [' . $language . '].']], $input, $params);
     }
 
     if (empty($content) === false) {
@@ -79,8 +79,8 @@ class YZhanTranslator {
     return null;
   }
 
-  public function detect(string $input, ?array $languages, ?array $params = array()) {
-    list($content, $params) = $this->run(array(array("type" => "text", "text" => 'Which of the following languages is the content in: ' . implode(',', $languages) . ', etc.? Output only one language name.')), $input, $params);
+  public function detect(string $input, ?array $languages, ?array $params = []) {
+    list($content, $params) = $this->run([["type" => "text", "text" => 'Which of the following languages is the content in: ' . implode(',', $languages) . ', etc.? Output only one language name.']], $input, $params);
 
     if (empty($content) === false) {
       return $content;
